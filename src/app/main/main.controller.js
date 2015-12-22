061d1d24a24af029716ae5726240dcde998eb15c
Raja <r4rajaa@gmail.com>
2015-12-21 17:16:55 -0800

    completed login

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,10 +3,25 @@
 
   angular
     .module('road2math')
-    .controller('MainController', MainController);
+    .config(function($stateProvider, $urlRouterProvider){
+
+      $stateProvider
+        //.state('login', {
+        //  url: "/login",
+        //  templateUrl: "login.html",
+        //  controller: "LoginCheckController"
+        //})
+        .state('Dashboard', {
+          url: "/Dashboard",
+          templateUrl: "app/dashboard/dashboard.html",
+          //controller: "LoginCheckController"
+        });
+
+    })
+    .controller('MainController', ['$scope', '$location', MainController]);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($scope, $location) {
     var vm = this;
     vm.a = "abc";
     vm.mentor_login_display = false;
@@ -75,50 +90,58 @@
       vm.student_login_list = false;
       vm.student_password = true;
     }
+
+
+    vm.login_mentor = function(){
+      console.log('login');
+      $location.path("Dashboard")
+
+    }
+
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1448582199696;
-    vm.showToastr = showToastr;
+    //vm.showToastr = showToastr;
 
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-      console.log(vm.awesomeThings);
-    }
-
-    vm.myInterval = 3000;
-    vm.slides = [
-      {
-        image: 'http://lorempixel.com/400/200/'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/food'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/sports'
-      },
-      {
-        image: 'http://lorempixel.com/400/200/people'
-      }
-    ];
+    //
+    //activate();
+    //
+    //function activate() {
+    //  getWebDevTec();
+    //  $timeout(function() {
+    //    vm.classAnimation = 'rubberBand';
+    //  }, 4000);
+    //}
+    //
+    //function showToastr() {
+    //  toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    //  vm.classAnimation = '';
+    //}
+    //
+    //function getWebDevTec() {
+    //  vm.awesomeThings = webDevTec.getTec();
+    //
+    //  angular.forEach(vm.awesomeThings, function(awesomeThing) {
+    //    awesomeThing.rank = Math.random();
+    //  });
+    //  console.log(vm.awesomeThings);
+    //}
+    //
+    //vm.myInterval = 3000;
+    //vm.slides = [
+    //  {
+    //    image: 'http://lorempixel.com/400/200/'
+    //  },
+    //  {
+    //    image: 'http://lorempixel.com/400/200/food'
+    //  },
+    //  {
+    //    image: 'http://lorempixel.com/400/200/sports'
+    //  },
+    //  {
+    //    image: 'http://lorempixel.com/400/200/people'
+    //  }
+    //];
 
   }
 })();
